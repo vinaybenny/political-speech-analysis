442aec6c96c854c0c0d95ad4449f181fd2afe266
vinay.benny <vinaybenny@gmail.com>
2019-04-25 02:48:10 +1200

    fixed issues with checking if the content is in english or not. used to have issues with special unicode characters like non-breaking space.

--- preprocess/lang_translator.py
+++ preprocess/lang_translator.py
@@ -2,9 +2,20 @@ import json
 import os
 import time
 from googletrans import Translator
+import numpy.random
+import unicodedata as ud
 
-GTRANS_QUERY_DELAY = 2
-DATA_DIRECTORY = "./extract_speech/data"
+latin_letters= {}
+GTRANS_QUERY_DELAY = 3
+DATA_DIRECTORY = "../extract_speech/data"
+
+def is_latin(uchr):
+    try: return latin_letters[uchr]
+    except KeyError:
+         return latin_letters.setdefault(uchr, 'LATIN' in ud.name(uchr))
+
+def only_roman_chars(unistr):
+    return all(is_latin(uchr) for uchr in unistr if uchr.isalpha()) # isalpha suggested by John Machin
 
 def isEnglish(s):
     try:
@@ -14,12 +25,34 @@ def isEnglish(s):
     else:
         return True
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 if __name__== "__main__":
 
     rejected_files = []
     directory = DATA_DIRECTORY
 
-    for filename in os.listdir(directory):        
+    for filename in os.listdir(directory):
+        print("Starting translation for file %s" % filename)
         with open(directory + '/' + filename, encoding='utf-8') as json_file:  
             data = json.load(json_file)        
             sentences = data['content'].split('।')
@@ -29,7 +62,7 @@ if __name__== "__main__":
 
             for sentence in sentences:
                 # Check is the full sentence is in English. If so, no translation is required.
-                if isEnglish(sentence):
+                if only_roman_chars(sentence):
                     translated_sentence = sentence
                 else:
                     # Check whether the number of characters in the sentence is longer than 12k. If so, abort translation of current file.
@@ -40,17 +73,27 @@ if __name__== "__main__":
                     # Apply Google translate to sentence
                     translator = Translator()
                     translated_sentence = translator.translate(sentence).text
-                    time.sleep(GTRANS_QUERY_DELAY)
+
+                    # Add a bit of (capped) randomness to the time delay between successive google translate queries.
+                    wait_delay= round(numpy.random.normal(GTRANS_QUERY_DELAY, 2),1)
+                    if wait_delay < 1.9:
+                        wait_delay = 1.9
+                    elif wait_delay > 7.3:
+                        wait_delay = 7.3
+                    else:
+                        pass
+                    time.sleep(wait_delay)
                 
                 #print(sentence)
                 #print(translated_sentence)
                 translated_list.append(translated_sentence)
-                print("%s / %s" % (iter_counter, sentence_count))                            
+                printProgressBar(iter_counter, sentence_count, prefix = 'Progress:', suffix = 'Complete', length = 50)
+                #print("%s / %s" % (iter_counter, sentence_count))                            
                 iter_counter += 1
 
         translated_data = ". ".join(translated_list)
         data['translated_data'] = translated_data
-        print(translated_data)
+        #print(translated_data)
 
         with open(directory + '_translated/' + filename, 'w', encoding='utf-8') as datafile:
             json.dump(data, datafile, ensure_ascii=False)

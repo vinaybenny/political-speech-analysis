33647da6bd4afc3ef37584bef5e9aff5e09413ea
vinay.benny <vinaybenny@gmail.com>
2019-05-20 18:14:16 +1200

    Checkpoint before rewriting script to perform Dynamic Topic Modelling

--- preprocess/textprocessing.py
+++ preprocess/textprocessing.py
@@ -74,21 +74,31 @@ def replace_numbers(words):
 def remove_stopwords(words):
     """Remove stop words from list of tokenized words"""
     new_words = []
+
+    # Set up NLTK's stop word corpus.
     stop_words = stopwords.words('english')
-    
+
     # List extra words to be discarded from tokens
-    stop_words.extend(['ji', 'twitter', 'year', 'brother', 'sister', 'work', 'today', 'time', 'work', 'friend', 'day', 'minister', 'thing'\
-        ,'way', 'companion', 'crore', 'mother', 'man', 'lot', 'hand', 'colleague', 'lakh', 'month', 'number', 'name', 'half', 'someone'\
-        ,'country', 'people', 'government', 'india'\
+    stop_words.extend(['ji', 'twitter'\
+        , 'come', 'today', 'work', 'year', 'brother_sister', 'time', 'go', 'give', 'tell', 'say', 'want', 'get', 'day', 'thing', 'brother'\
+        , 'way', 'see', 'crore', 'bring', 'take', 'man', 'lot', 'hand'\
+        , 'country', 'people', 'government', 'india'\
             ])
+
+    # Set up spacy's stop word list
+    nlp = en_core_web_sm.load()
+
+    # Create a combination of spacy and nltk stop words
+    nlp.Defaults.stop_words |= {word for word in stop_words}
+
     for word in words:
-        if word not in stop_words:
+        if not(nlp.vocab[word].is_stop):
             new_words.append(word)
     return new_words
 
 def make_ngrams(docs):
-    bigram = gensim.models.Phrases(docs, min_count=3, threshold=10)
-    trigram = gensim.models.Phrases(bigram[docs], min_count=3, threshold=10)
+    bigram = gensim.models.Phrases(docs, min_count=3, threshold=5)
+    trigram = gensim.models.Phrases(bigram[docs], min_count=3, threshold=5)
 
     bigram_phraser = gensim.models.phrases.Phraser(bigram)
     trigram_phraser = gensim.models.phrases.Phraser(trigram)
@@ -113,9 +123,8 @@ def normalize(words):
     
     words = remove_non_ascii(words)
     words = remove_punctuation(words)
-    words = replace_numbers(words)
-    words = lemmatize(words, allowed_postags=['NOUN'])
-    words = remove_stopwords(words)
+    words = replace_numbers(words)    
+    words = lemmatize(words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])  
     return words
 
 
@@ -139,15 +148,13 @@ def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
     model_list = []
     for num_topics in range(start, limit, step):
         print("Number of Topics is {}".format(num_topics))
-        model = LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary, chunksize=2000, passes=1)
+        model = LdaModel(corpus=corpus, num_topics=num_topics, alpha='auto', eta='auto', id2word=dictionary, chunksize=2000, passes=50)
         model_list.append(model)
         coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
         print("Coherence score is {}".format(coherencemodel.get_coherence()))
         print('Perplexity: ', model.log_perplexity(corpus))
         coherence_values.append(coherencemodel.get_coherence())
-
     return model_list, coherence_values
-
 
 if __name__== "__main__":
 
@@ -170,11 +177,37 @@ if __name__== "__main__":
     # Apply data pre-processing & lemmatisation    
     filedata['cleaned_data'] = filedata['translated_data'].apply(normalize)
     filedata['cleaned_data'] = make_ngrams(filedata['cleaned_data'])
+    filedata['lemmatised_data'] = filedata['cleaned_data'].apply(remove_stopwords)
 
-    dictionary = Dictionary(filedata['cleaned_data'])
-    corpus = [dictionary.doc2bow(text) for text in filedata['cleaned_data']]
+    #TODO: Perform a spell-correction on the words
+    #TODO: Remove words that occur only once (after spell correction)
+
+    # Try overall topic detection first
+    dictionary = Dictionary(filedata['lemmatised_data'])
+    corpus = [dictionary.doc2bow(text) for text in filedata['lemmatised_data']]
+    lm_list, coherence_values = compute_coherence_values(dictionary, corpus, filedata['lemmatised_data'], limit = 30, step = 2)
+    # lm_list[np.argmax(coherence_values)]
+
+
+    # Try batchwise topic detection
+    batchsize = 10
+    best_models=[]
+    for batchnum in range(0, len(filedata['lemmatised_data']) - batchsize):
+        dictionary = Dictionary(filedata[batchnum:(batchnum + batchsize)]['lemmatised_data'])
+        corpus = [dictionary.doc2bow(text) for text in filedata[batchnum:(batchnum + batchsize)]['lemmatised_data']]
+        model_list, coherence_measures = compute_coherence_values(dictionary, corpus, filedata[batchnum:(batchnum + batchsize)]['lemmatised_data'], limit = 10, step = 2)
+        best_models.append(model_list[np.argmax(coherence_measures)])
+
     
-    lm_list, coherence_values = compute_coherence_values(dictionary, corpus, filedata['cleaned_data'], limit = 30, step = 2)
+    
+
+
+# import pickle
+# with open('.\\data\\best_models.pkl', 'wb') as output:
+#     pickle.dump(best_models, output, pickle.HIGHEST_PROTOCOL)
+
+# with open('.\\data\\best_models.pkl', 'rb') as input:
+#     best_models = pickle.load(input)
 
 # limit=30
 # start=2
@@ -186,21 +219,11 @@ if __name__== "__main__":
 # plt.legend(("coherence_values"), loc='best')
 # plt.show()
 
-# # for i in range(0, 13177):
-# #     if(dictionary[i] == "consciously"):
-# #         print("index is {}".format(i))
-# pprint(lm_list[13].print_topics())
-# doc_lda = lm_list[11][corpus]
-# model = LdaModel(corpus=corpus, num_topics=12, id2word=dictionary, chunksize=2000, passes=1)
+# pprint(lm_list[8].print_topics(num_words=20))
+# # doc_lda = lm_list[11][corpus]
+# # model = LdaModel(corpus=corpus, num_topics=12, id2word=dictionary, chunksize=2000, passes=1)
 
-# thestring = "text1\ntext2\npic. twitter. com/bla1/blah1/\ntext3\ntext4\nhttp://url.com/bla2/blah2/\ntext5\ntext6"
-#URLless_string = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', thestring)
-#URLless_string = re.sub(r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\S', '', thestring)
-# URLless_string = re.sub(r'\s+', ' ', thestring)
-# print(URLless_string)
-# URLless_string = re.sub(r' co(m|\.[a-z]*)\/(\/|\S+)|twitter.|pic.', '', URLless_string)
-# print(URLless_string)
-
+# # Check most frequent words
 # import itertools
 # from collections import defaultdict
 
@@ -208,4 +231,18 @@ if __name__== "__main__":
 # for word_id, word_count in itertools.chain.from_iterable(corpus):
 #     total_count[dictionary[word_id]] += word_count
 # # Top ten words
-# sorted(total_count.items(), key=lambda x: x[1], reverse=True)[:70]
+# sorted(total_count.items(), key=lambda x: x[1], reverse=False)[:100]
+
+# once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+
+# frequency = defaultdict(int)
+# for text in filedata['cleaned_data']:
+#      for token in text:
+#          frequency[token] += 1
+
+# texts = [
+#      [token for token in text if frequency[token] == 1]
+#      for text in filedata['cleaned_data']
+#  ]
+
+# print(texts)
